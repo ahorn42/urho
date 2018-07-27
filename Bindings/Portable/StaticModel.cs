0fe38ee96298103b10e4636a0f8ad33dd634e518
Andreas Horn <ahhorn@atos.net>
2018-07-27 09:28:35 +0200

    added further implementations in StaticModel

--- Bindings/Portable/StaticModel.cs
+++ Bindings/Portable/StaticModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace Urho
 {
 	partial class StaticModel
@@ -7,7 +10,29 @@ namespace Urho
 			get { return GetMaterial(0); }
 			set { SetMaterial(0, value); }
 		}
-	}
+
+        [DllImport("mono-holourho", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr StaticModel_ProcessRayQuery(IntPtr handle, ref Ray ray, ref RayQueryLevel level, float maxDistance, uint drawableFlags, uint viewMask);
+
+        public RayQueryResult ProcessRayQuery(Ray ray, RayQueryLevel level, float maxDistance, DrawableFlags drawableFlags, uint viewMask = UInt32.MaxValue) {
+            //Runtime.ValidateRefCounted(this);
+
+            var result = new RayQueryResult();
+
+            var ptr = StaticModel_ProcessRayQuery(Handle, ref ray, ref level, maxDistance, (uint)drawableFlags, viewMask);
+
+            //handle
+
+            if (ptr == IntPtr.Zero) {
+                return result;
+            }
+
+            IntPtr data = new IntPtr(ptr.ToInt64());
+            RayQueryResult item = (RayQueryResult)Marshal.PtrToStructure(data, typeof(RayQueryResult));
+
+            return result;
+        }
+    }
 
 	partial class AnimatedModel
 	{
